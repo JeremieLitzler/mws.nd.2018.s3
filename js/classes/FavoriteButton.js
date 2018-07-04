f6574edd14bc74b57f806dfceaee7b8f49fde079
Jeremie Litzler <jeremie.litzler@conduent.com>
2018-07-04 13:54:39 +0200

    #5-feat: change text to svg image

--- js/classes/FavoriteButton.js
+++ js/classes/FavoriteButton.js
@@ -1,5 +1,6 @@
 const IS_FAVORITE_CLASS = "is-favorite";
 const ISNT_FAVORITE_CLASS = "isnt-favorite";
+const START_CLASS = "star";
 
 class FavoriteButton {
   constructor(favoriteButton) {
@@ -21,12 +22,27 @@ class FavoriteButton {
   UpdateAsNotFavorite() {
     this.TheElement.classList.remove(IS_FAVORITE_CLASS);
     this.TheElement.classList.add(ISNT_FAVORITE_CLASS);
+    this.RemoveStarSvg();
     this.TheElement.textContent = "Set as your favorite";
   }
 
   UpdateAsFavorite() {
     this.TheElement.classList.remove(ISNT_FAVORITE_CLASS);
     this.TheElement.classList.add(IS_FAVORITE_CLASS);
-    this.TheElement.textContent = "My favorite!";
+    const starImgElement = document.createElement("img");
+    starImgElement.className = START_CLASS;
+    starImgElement.tabIndex = 0;
+    starImgElement.src = "./img/icons/fa/star-full.svg";
+    const isMyFavoriteText = "This restaurant is my restaurant";
+    starImgElement.alt = isMyFavoriteText;
+    starImgElement.title = isMyFavoriteText;
+    this.TheElement.textContent = "";
+    this.TheElement.appendChild(starImgElement);
+  }
+  RemoveStarSvg() {
+    const starImgElement = document.querySelector(`.${START_CLASS}`);
+    if (starImgElement != null) {
+      this.TheElement.removeChild(starImgElement);
+    }
   }
 }
